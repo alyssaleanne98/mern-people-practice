b958edcd912cb9643fd2dede451f85cf089bbe8d
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-07-20 22:05:05 -0700

    components and pages created

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+// Write the component boilerplate and export the component in all the created files
+
+function Component(proper) {
+    return <h1> Component name</h1>
+
+
+}
+
+export default Component;
---
+++ components/Main.js
@@ -0,0 +1,7 @@
+// Component boilerplate
+
+function Main (props) {
+    return <h1> Main component</h1>
+}
+
+export default Main;
