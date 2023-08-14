2886391689638df5ce7c4b4586e28deeda51f200
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-08-14 15:39:37 -0700

    display people in index

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,45 @@
 // Component boilerplate
+import { useEffect, useState } from "react"
+// Image this as a toolkit. We are taking out two specific tools that make it easier to manage certain aspects of the website or app: useEffect and useState. `useEffect` is a tool that helps you manage when certain actions happen. `useState` is a tool that helps you keep track of information that can change over time. 
 import { Routes, Route } from "react-router-dom"
-import Index from "../pages/Index"; 
-import Show from "../pages/Show"; 
+import Index from "../pages/Index";
+import Show from "../pages/Show";
 
 
 
 function Main(props) {
+    const [people, setPeople] = useState(null)
+  
+    const URL = "http://localhost:3001/people/"
+  
+    const getPeople = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+      setPeople(data)
+    }
+  
+    const createPeople = async (person) => {
+      // make post request to create people
+      await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(person),
+      })
+      // update list of people
+      getPeople()
+    }
+  
+    useEffect(() => getPeople(), [])
     return (
-    <main>
-    
-        <Routes>
-            <Route exact path="/" element={<Index />} />
-            <Route path="/people/:id" element={<Show />} />
-        </Routes>
-    </main>
+        <main>
+
+            <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Show />} />
+            </Routes>
+        </main>
     )
 }
 
@@ -31,4 +57,11 @@ export default Main;
 // The 'react-router-dom' library provides two components: 'Routes' and 'Route'. The "Routes" component is used as a container for individual 'Route' components, which define the mappings between specific URL paths and the components that should be rendered when those paths are matched. 
 
 // Route Definitions: 
-//
+
+
+// Updated Main to have: 
+    // State to hold our list of people
+    // function to make the api call for people
+    // function to create a new person
+    // useEffect to make initial call for people list
+    // pass the people state and the create function to index 
