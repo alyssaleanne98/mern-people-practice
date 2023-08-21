9a3c3675b505be78ca798bb5c257969e941cd2d8
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-08-21 16:51:44 -0700

    main.js debugge

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,42 +8,40 @@ import Show from "../pages/Show";
 
 
 function Main(props) {
-    const [people, setPeople] = useState(null)
-  
-    const URL = "http://localhost:3001/people/"
-  
-    const getPeople = async () => {
-      const response = await fetch(URL)
-      const data = await response.json()
-      setPeople(data)
-    }
-  
-    const createPeople = async (person) => {
-      // make post request to create people
-      await fetch(URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "Application/json",
-        },
-        body: JSON.stringify(person),
-      })
-      // update list of people
-      getPeople()
-    }
-  
-    useEffect(() => getPeople(), [])
-    return (
-        <main>
+  const [people, setPeople] = useState(null)
 
-            <Routes>
-                <Route exact path="/" element={<Index />} />
-                <Route path="/people/:id" element={<Show />} />
-            </Routes>
-        </main>
-    )
+  const URL = "http://localhost:3001/people/"
+
+  const getPeople = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setPeople(data)
+  }
+
+  const createPeople = async (person) => {
+    // make post request to create people
+
+  }
+
+  useEffect(() => {
+    getPeople()
+  }, [])
+
+  return (
+    <main>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Index people={people} createPeople={createPeople} />}
+        />
+        <Route path="/people/:id" element={<Show />} />
+      </Routes>
+    </main>
+  )
 }
 
-export default Main;
+export default Main
 
 //Notes:
 
@@ -64,22 +62,24 @@ export default Main;
     // function to make the api call for people
     // function to create a new person
     // useEffect to make initial call for people list
-    // pass the people state and the create function to index 
+    // pass the people state and the create function to index
 
         // Functionc component declaration
-            //`function Main (props)` declares a function component called 'Main' that can accept `properties` as its input. Props are used to pass data into components. 
-        // state and useState 
-            // `const [people, setPeople] = useState(null)` uses the `useState` hook to declare a piece of state called `people` and a function called `setPeople` to update that state. The initial value of `people` is set to null. WHY? 
-                // asynchronous data fetching: when your component is fetching data from an external source (like an API), there is a period of time during which the data is being fetched. Until its available, you might not want to display anything specific on the UI related to the data. 
-                // conditional rendering: to control what gets displayed based on the state of the data. for example, you might want to show a loading spinner while the data is being fetched, and then display the actual data once it's available. 
+            //`function Main (props)` declares a function component called 'Main' that can accept `properties` as its input. Props are used to pass data into components.
+        // state and useState
+            // `const [people, setPeople] = useState(null)` uses the `useState` hook to declare a piece of state called `people` and a function called `setPeople` to update that state. The initial value of `people` is set to null. WHY?
+                // asynchronous data fetching: when your component is fetching data from an external source (like an API), there is a period of time during which the data is being fetched. Until its available, you might not want to display anything specific on the UI related to the data.
+                // conditional rendering: to control what gets displayed based on the state of the data. for example, you might want to show a loading spinner while the data is being fetched, and then display the actual data once it's available.
 
     //URL and Fetch Functions:
-        // const URL = "http://localhost:3001/people/" defines the url that will be used to fetch and send data. 
-        // ` getPeople` fetches data from the defined URL, converts the response to JSON format, and updates the `people` state with the fetched data. 
-        // `createPeople` sends a POST request to create new people on the server. It then calls `getPeople` to update the list of people after creating a new entry. 
+        // const URL = "http://localhost:3001/people/" defines the url that will be used to fetch and send data.
+        // ` getPeople` fetches data from the defined URL, converts the response to JSON format, and updates the `people` state with the fetched data.
+        // `createPeople` sends a POST request to create new people on the server. It then calls `getPeople` to update the list of people after creating a new entry.
         // `useEffect` is a hook that runs side effects (such as fetching data) in function components. In this case, it's used to fetch the list of people when the component mounts (when it's first rendered)
         // the empty array `[]` at the end of `useEffect` means that this effect will run only once, right after the component is mounted. (referring to the process of a component being created and inserted into the DOM for the first time)
 
-  // Return JSX: 
-      // the `return` statement defines the JSX (JS XML) that will be rendered by the component. 
-      // Inside JSX, the `Routes` and `Route` components are used to define routing for different URLs. When the URL matches a specified path, it renders the corresponding element. 
+  // Return JSX:
+      // the `return` statement defines the JSX (JS XML) that will be rendered by the component.
+      // Inside JSX, the `Routes` and `Route` components are used to define routing for different URLs. When the URL matches a specified path, it renders the corresponding element. (index and show components)
+  // Export component:
+      // export default Main: makes the `Main` component available for other parts of your application to import use. 
