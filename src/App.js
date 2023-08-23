11fbf9c28de1e3c52244872c47337bca2f8dc818
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-08-22 21:55:32 -0700

    notes added app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Main from "./components/Main"
 
 //The main component of our application called "App"
 function App() {
+  // To invoke or call its function means using the component's name (App) followed by empty parentheses - to make the component visible (or essentially creating a blueprint for UI element)
   // The return statement below defines what our component should display.
   // It's like saying "When someone uses this `App`component, show this content."
   return (
@@ -20,4 +21,5 @@ function App() {
   );
 }
 
+//This line exports the app component so other parts of the app can use it
 export default App;
