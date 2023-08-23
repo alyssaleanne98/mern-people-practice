44250aa93d54f6f0524cf40a150e5b6eff56371c
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-08-22 21:47:37 -0700

    notes added App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
+// imports the styles from app.css file
 import Header from "./components/Header"
 import Main from "./components/Main"
+// import the Header and Main components 
 
 
 
-
+//The main component of our application called "App"
 function App() {
+  // The return statement below defines what our component should display.
+  // It's like saying "When someone uses this `App`component, show this content."
   return (
     <div className="App">
       
